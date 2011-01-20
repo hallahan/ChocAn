182277f600826f85c7208db8713b02b48fcfa23a
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-01-20 12:43:35 -0800

    second commit
    Signed-off-by: Nicholas Hallahan <nicholashallahan@gmail.com>

--- src/HelloGit/HelloGit.java
+++ src/HelloGit/HelloGit.java
@@ -2,12 +2,10 @@ package HelloGit;
 
 public class HelloGit {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
+		System.out.println("Here is the second commit.");
 	}
 
 }
