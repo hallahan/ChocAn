0499a26ed188016c7aae05dedcb01d84b3bab024
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-01-20 15:53:41 -0800

    began the StartApplication class that guides the application flow - JOptionPane to retrieve Provider ID
    Signed-off-by: Nicholas Hallahan <nicholashallahan@gmail.com>

---
+++ src/controller/StartApplication.java
@@ -0,0 +1,17 @@
+package controller;
+import model.*;
+import javax.swing.JOptionPane;
+
+public class StartApplication {
+	public static void main(String[] args) {
+		login();
+	}
+
+	private static void login() {
+		String id = JOptionPane.showInputDialog(null, "Enter your Provider ID Number:", "Enter ID", JOptionPane.PLAIN_MESSAGE);
+		if (id != null)
+			System.out.println(id);
+		else
+			System.out.println("id is null");
+	}
+}
