c50efef1a3763eb085f31f5bba506d3d151debe8
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-01-20 12:41:24 -0800

    testing git first commit
    Signed-off-by: Nicholas Hallahan <nicholashallahan@gmail.com>

---
+++ src/HelloGit/HelloGit.java
@@ -0,0 +1,13 @@
+package HelloGit;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+	}
+
+}
