26171515090e84fd1358dbdc65be6c6ee4086778
Nicholas Hallahan <nicholashallahan@gmail.com>
2011-01-20 15:04:05 -0800

    So now we merged with the PC by adding the HelloGit test lines.
    Signed-off-by: Nicholas Hallahan <nicholashallahan@gmail.com>

--- src/HelloGit/HelloGit.java
+++ src/HelloGit/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Here is the second commit.");
+		System.out.println("Here is more material.");
 	}
 
 }
