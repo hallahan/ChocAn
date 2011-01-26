b76eea66bd29e1f3879e6e71465c0e869993d7af
hallahan <nicholashallahan@gmail.com>
2011-01-26 00:07:26 -0800

    implemented EMPTY constant in Member.java no longer wasting space with a bunch of empty strings inside instances of the Member class...
    Signed-off-by: hallahan <nicholashallahan@gmail.com>

--- src/model/Member.java
+++ src/model/Member.java
@@ -3,6 +3,9 @@ import java.lang.Integer;
 
 
 public class Member {
+	private static final String EMPTY = "";
+	private static Integer defaultActiveStatus = 1;
+	
 	private Integer	member_id;     //db automatically increments id if supplied with null
 	private Integer	active_status; //1 is active, 0 is inactive
 	private String	first;
@@ -32,110 +35,113 @@ public class Member {
  	public void print() {
 		System.out.println("--------------------");
 		System.out.println(member_id.toString());
-		if (active_status == 1) System.out.println("active");
-		else System.out.println("inactive");
-		System.out.println(first);
-		System.out.println(middle);
-		System.out.println(last);
-		System.out.println(address);
-		System.out.println(city);
-		System.out.println(state);
-		System.out.println(zip);
+		if (this.getActive_status() == true) 
+			System.out.println("active");
+		else 
+			System.out.println("inactive");
+		System.out.println(this.getFirst());
+		System.out.println(this.getMiddle());
+		System.out.println(this.getLast());
+		System.out.println(this.getAddress());
+		System.out.println(this.getCity());
+		System.out.println(this.getState());
+		System.out.println(this.getZip());
 	}
 	
 	
 	
 	//setters and getters
 	public Integer getMember_id() {
-		return member_id;
+		if (this.member_id == null)
+			return 0;
+		else
+			return this.member_id;
 	}
 	public void setMember_id(Integer member_id) {
 		//we want this to be null if set to null
 		this.member_id = member_id;
 	}
-	public Integer getActive_status() {
-		return active_status;
-	}	
-	public boolean getActive_statusBool() {
+	public boolean getActive_status() {
 		if (active_status == 1)
 			return true;
 		else
 			return false;
-	}
+	}	
+
 	public void setActive_status(Integer active_status) {
 		if (active_status == null)
-			this.active_status = 1;
+			this.active_status = defaultActiveStatus;
 		else
 			this.active_status = active_status;
 	}
-	public void setActive_statusBool(boolean tf) {
+	public void setActive_status(boolean tf) {
 		if (tf == true)
 			active_status = 1;
 		else
 			active_status = 0;
 	}	
 	public String getFirst() {
-		return first;
+		if (this.first == null)
+			return EMPTY;
+		else
+			return this.first;
 	}
 	public void setFirst(String first) {
-		if (first == null)
-			this.first = "";
-		else
-			this.first = first;
+		this.first = first;
 	}
 	public String getMiddle() {
-		return middle;
+		if (this.middle == null)
+			return EMPTY;
+		else
+			return this.middle;
 	}
 	public void setMiddle(String middle) {
-		if (middle == null)
-			this.middle = "";
-		else
-			this.middle = middle;
+		this.middle = middle;
 	}
 	public String getLast() {
-		return last;
+		if (this.last == null)
+			return EMPTY;
+		else
+			return this.last;
 	}
 	public void setLast(String last) {
-		if (this.last == null)
-			this.last = "";
-		else
-			this.last = last;
+		this.last = last;
 	}
 	public String getAddress() {
-		return address;
+		if (this.address == null)
+			return EMPTY;
+		else
+			return this.address;
 	}
 	public void setAddress(String address) {
-		if (address == null)
-			this.address = "";
-		else
-			this.address = address;
+		this.address = address;
 	}
 	public String getCity() {
-		return city;
+		if (this.city == null)
+			return EMPTY;
+		else
+			return city;
 	}
 	public void setCity(String city) {
-		if (city == null)
-			this.city = "";
-		else
-			this.city = city;
+		this.city = city;
 	}
 	public String getState() {
-		return state;
+		if (this.state == null)
+			return EMPTY;
+		else
+			return state;
 	}
 	public void setState(String state) {
-		if (state == null)
-			this.state = "";
-		else
-			this.state = state;
+		this.state = state;
 	}
 	public String getZip() {
-		return zip;
+		if (this.zip == null)
+			return EMPTY;
+		else
+			return this.zip;
 	}
 	public void setZip(String zip) {
-		if (zip == null)
-			this.zip = "";
-		else
-			this.zip = zip;
+		this.zip = zip;
 	}
 
 }
